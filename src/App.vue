8334d08f4cd0cc6cf2c8ab4fa7f67fbe4e722529
MichealAdeotun <adedotun2096@gmail.com>
2023-03-23 13:56:37 +0100

    added app.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import AgrovestoHeader from "./components/AgrovestoHeader.vue";
+  import AgrovestoFooter from "./components/AgrovestoFooter.vue";
+
+</script>
+
+<template>
+    <AgrovestoHeader />
+    <router-view />
+    <AgrovestoFooter />
+</template>
+
+<style scoped>
+
+</style>
