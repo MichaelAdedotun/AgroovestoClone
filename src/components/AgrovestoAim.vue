c87cab34c184f2ca1e2f80cefaa081b32f2147ba
MichealAdeotun <adedotun2096@gmail.com>
2023-11-25 21:43:54 +0100

    mobile section

--- src/components/AgrovestoAim.vue
+++ src/components/AgrovestoAim.vue
@@ -1,21 +1,22 @@
 <template>
-    <main class="bg-aim-bg bg-no-repeat bg-cover bg-fixed bg-dark-gray px-28 py-24">
-      <div class="text-white mb-16">
-        <h1 class="text-5xl font-bold mr-56 pr-48">
-          Building a Sustainable
+    <main class="bg-aim-bg bg-no-repeat bg-cover bg-fixed bg-dark-gray py-14 px-6 md:px-8 lg:px-16 xl:px-20 md:py-24">
+      <div class="text-white md:mb-16">
+        <h1 class="text-2xl font-bold md:text-4xl xl:text-5xl">
+          Building a Sustainable <br>
           Agriculture for a Better World.
         </h1>
-        <p class="pt-6 text-sm mr-48 pr-44">
+        <p class="py-4 text-xs md:py-6 md:text-sm">
           Welcome to Agrovesto, a platform built for individuals 
           and Businesses to thrive and succeed in African Agricultural 
-          Sector, make food Available and Affordable for everyone
+          Sector, <br>
+          make food Available and Affordable for everyone
         </p>
       </div>
 
-      <section class="mt-5 grid grid-cols-4">
-        <div class="bg-white mr-3">
+      <section class="my-5 grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+        <div class="bg-white">
           <div class="relative">
-            <img class="h-52" src="/src/assets/images/farm1.png" alt="farm picture">
+            <img class="h-52 w-full object-cover" src="/src/assets/images/farm1.png" alt="farm picture">
             <router-link to="">
               <span class="absolute bottom-0 right-0 bg-green-700 text-white font-semibold px-4 py-1 ">
                 BUY NOW
@@ -33,9 +34,9 @@
           </div>
         </div>
 
-        <div class="bg-white mx-3">
+        <div class="bg-white">
           <div class="relative">
-            <img class="h-52" src="/src/assets/images/farm2.png" alt="farm picture">
+            <img class="h-52 w-full object-cover" src="/src/assets/images/farm2.png" alt="farm picture">
             <router-link to="">
               <span class="absolute bottom-0 right-0 bg-green-700 text-white font-semibold px-4 py-1">
                 TRADE NOW
@@ -54,9 +55,9 @@
           </div>
         </div>
 
-        <div class="bg-white mx-3">
+        <div class="bg-white">
            <div class="relative">
-            <img class="h-52" src="/src/assets/images/farm3.png" alt="farm picture">
+            <img class="h-52 w-full object-cover" src="/src/assets/images/farm3.png" alt="farm picture">
             <router-link to="">
               <span class="absolute bottom-0 right-0 bg-green-700 text-white font-semibold px-4 py-1">
                 REQUEST NOW
@@ -74,9 +75,9 @@
           </div>
         </div>
 
-        <div class="bg-white mx-3">
+        <div class="bg-white">
            <div class="relative">
-            <img class="h-52" src="/src/assets/images/farm4.png" alt="farm picture">
+            <img class="h-52 w-full object-cover" src="/src/assets/images/farm4.png" alt="farm picture">
             <router-link to="">
               <span class="absolute bottom-0 right-0 bg-green-700 text-white font-semibold px-4 py-1">
                 JOIN NOW
@@ -86,7 +87,7 @@
           </div>
           <div class="p-5">
             <line class="flex border border-yellow-400 w-12 h-1 bg-yellow-400 mb-2"></line>
-            <h2 class="text-gray-900 text-lg font-bold mb-5 leading-none">FARM PARTNER'S' COMMUNITY</h2>
+            <h2 class="text-gray-900 text-lg font-bold mb-5 leading-none">FARM PARTNER'S COMMUNITY</h2>
             <p class="text-gray-500 text-sm mb-6">
               We work with large community of smallholder farmers in Nigeria,
               you can join and make huge profit as a partner in crop production.
